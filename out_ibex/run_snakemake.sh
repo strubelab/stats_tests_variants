325ae8106de54d8658bd3c262214b7cecbb09025
Francisco J. Guzman <36437485+guzmanfj@users.noreply.github.com>
2025-04-16 15:04:02 +0300

    Configure to run repeat

--- out_ibex/run_snakemake.sh
+++ out_ibex/run_snakemake.sh
@@ -3,13 +3,13 @@
 #SBATCH --partition=batch
 #SBATCH --job-name=stats
 #SBATCH --output=./out_ibex/%x.%j.out
-#SBATCH --time=12:00:00
-#SBATCH --mem=256G
-#SBATCH --cpus-per-task=36
-#SBATCH --mail-user=your.email@kaust.edu.sa
+#SBATCH --time=02:00:00
+#SBATCH --mem=32G
+#SBATCH --cpus-per-task=16
+#SBATCH --mail-user=francisco.guzmanvega@kaust.edu.sa
 #SBATCH --mail-type=ALL
 
 conda activate ./env
 
-snakemake --cores 36 --resources mem_mb=250000 --sdm conda
+snakemake --cores 16 --resources mem_mb=30000 --sdm conda
 
